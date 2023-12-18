50fe4420c2b0bc7a29d769d8b0ae2b87a2e91358
rawadabdallah <rawad0ne@gmail.com>
2023-12-18 20:14:47 +0200

    [core] Auth setup

---
+++ backend/models/user.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required."],
+        unique: true,
+        minlength: [3, "Email must be at least 3 characters."],
+        maxlength: [20, "Email must be at most 20 characters."],
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."],
+        minlength: [6, "Password must be at least 6 characters."],
+    },
+    firstname: {
+        type: String,
+        required: [true, "First name is required."],
+        minlength: [2, "First name must be at least 6 characters."],
+    },
+    lastname: {
+        type: String,
+        required: [true, "Last name is required."],
+        minlength: [2, "Last name must be at least 6 characters."],
+    },
+});
+
+userSchema.pre("save", function (next) {
+    const validationErrors = this.validateSync();
+    if (validationErrors) {
+        // Extract the first validation error message and throw it
+        const firstErrorKey = Object.keys(validationErrors.errors)[0];
+        const firstErrorMessage =
+            validationErrors.errors[firstErrorKey].message;
+        next(new Error(firstErrorMessage));
+    } else {
+        next();
+    }
+});
+
+userSchema.pre(
+    "save",
+    async function (next) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+            next();
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
